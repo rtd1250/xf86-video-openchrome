685be184d4cd79f6a6e5cb1825cfb202b88683f1
Xavier Bachelot <xavier@bachelot.org>
2008-01-18 15:05:33 +0000

    Medion Notebook MD96483

--- src/via_id.c
+++ src/via_id.c
@@ -163,6 +163,7 @@ static struct ViaCardIdStruct ViaCardId[] = {
     /*** P4M900, VN896, CN896 ***/
     {"Asustek P5VD2-VM",                      VIA_P4M900,  0x1043, 0x81CE, VIA_DEVICE_CRT},
     {"Asustek P5VD2-VM SE",                   VIA_P4M900,  0x1043, 0x8252, VIA_DEVICE_CRT},
+    {"Medion Notebook MD96483",               VIA_P4M900,  0x1071, 0x8615, VIA_DEVICE_CRT | VIA_DEVICE_LCD},
     {"VIA VT3364 (P4M900)",                   VIA_P4M900,  0x1106, 0x3371, VIA_DEVICE_CRT | VIA_DEVICE_LCD},
     {"Gigabyte GA-VM900M",                    VIA_P4M900,  0x1458, 0xD000, VIA_DEVICE_CRT},
     {"MSI P4M900M / P4M900M2-F/L",            VIA_P4M900,  0x1462, 0x7255, VIA_DEVICE_CRT},
