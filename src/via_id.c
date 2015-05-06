2bc22ea40bd22618bb731accd1e30b8c453c4520
Xavier Bachelot <xavier@bachelot.org>
2015-05-07 00:23:40 +0200

    Abit IP-95 (Reported by Alan Polinsky)

--- src/via_id.c
+++ src/via_id.c
@@ -233,6 +233,7 @@ static struct ViaCardIdStruct ViaCardId[] = {
     {"Mitac 8624 (Joybook R41E)",             VIA_P4M890,  0x1071, 0x8624, VIA_DEVICE_CRT | VIA_DEVICE_LCD},
     {"VIA VT3343 (P4M890)",                   VIA_P4M890,  0x1106, 0x3343, VIA_DEVICE_CRT},
     {"MSI P4M890M-L/IL (MS-7255)",            VIA_P4M890,  0x1462, 0x7255, VIA_DEVICE_CRT},
+    {"Abit IP-95",                            VIA_P4M890,  0x147B, 0x1422, VIA_DEVICE_CRT},
     {"Biostar P4M890-M7 TE",                  VIA_P4M890,  0x1565, 0x1207, VIA_DEVICE_CRT},
     {"ASRock P4VM890",                        VIA_P4M890,  0x1849, 0x3343, VIA_DEVICE_CRT},
 
