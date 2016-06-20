rtd1250/xf86-video-openchrome

commit 19ad4151bf85b3fb8ff3be6b380d55c286bf43c9

Making small modifications to viaUnmapMMIO
Rearranging the code inside the function and adding additional
safeguards like setting the pointers NULL after the memory map is
released.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_ums.c
@@ -338,30 +338,40 @@ viaUnmapMMIO(ScrnInfoPtr pScrn)
     viaMMIODisable(pScrn);
 
 #ifdef HAVE_PCIACCESS
-    if (pVia->MapBase)
-        pci_device_unmap_range(pVia->PciInfo, (pointer) pVia->MapBase,
-                               VIA_MMIO_REGSIZE);
-
-    if (pVia->BltBase)
-        pci_device_unmap_range(pVia->PciInfo, (pointer) pVia->BltBase,
-                               VIA_MMIO_BLTSIZE);
-
-    if (pVia->FBBase)
+    if (pVia->FBBase) {
         pci_device_unmap_range(pVia->PciInfo, (pointer) pVia->FBBase,
                                pVia->videoRambytes);
+    }
+
+    if (pVia->BltBase) {
+        pci_device_unmap_range(pVia->PciInfo, (pointer) pVia->BltBase,
+                               VIA_MMIO_BLTSIZE);
+    }
+
+    if (pVia->MapBase) {
+        pci_device_unmap_range(pVia->PciInfo, (pointer) pVia->MapBase,
+                               VIA_MMIO_REGSIZE);
+    }
 #else
-    if (pVia->MapBase)
-        xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->MapBase,
-                        VIA_MMIO_REGSIZE);
-
-    if (pVia->BltBase)
-        xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->BltBase,
-                        VIA_MMIO_BLTSIZE);
-
-    if (pVia->FBBase)
+    if (pVia->FBBase) {
         xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->FBBase,
                         pVia->videoRambytes);
+    }
+
+    if (pVia->BltBase) {
+        xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->BltBase,
+                        VIA_MMIO_BLTSIZE);
+    {
+
+    if (pVia->MapBase) {
+        xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->MapBase,
+                        VIA_MMIO_REGSIZE);
+    }
 #endif
+
+    pVia->FBBase = NULL;
+    pVia->BltBase = NULL;
+    pVia->MapBase = NULL;
 
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Exiting viaUnmapMMIO.\n"));

commit 433e10d7aa948fa503a7b19cd007f50cb67d36e0

Fix when compiling in a build environment without libpciaccess
Commit 19ad415 introduced a syntax error if the code was compiled
in a build environment without libpciaccess. This was observed on
Ubuntu 8.04.4 LTS.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_ums.c
@@ -363,7 +363,7 @@ viaUnmapMMIO(ScrnInfoPtr pScrn)
     if (pVia->BltBase) {
         xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->BltBase,
                         VIA_MMIO_BLTSIZE);
-    {
+    }
 
     if (pVia->MapBase) {
         xf86UnMapVidMem(pScrn->scrnIndex, (pointer) pVia->MapBase,