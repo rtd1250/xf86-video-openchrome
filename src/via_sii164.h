45b88688c971233bbbb2c57ed93673d0cbdde228
Kevin Brace <kevinbrace@gmx.com>
2016-08-26 22:47:02 -0700

    Giving the original author of external TMDS transmitter code credit
    Luc Verhaegen notified that the code to support Silicon Image SiI 164
    and VIA Technologies VT1632(A) closely resembles the code he wrote
    for xf86-video-unichrome. After examining the code, portions of it
    appears to resemble Luc's original code. Christian Jung who was the
    author of the VIA Technologies VT1632(A) code acknowledged the lack
    of Luc's copyright in the code he submitted.
    
    Suggested-by: Luc Verhaegen <libv@skynet.be>
    Acked-by: JUNG, Christian <christian.jung@shsservices.org>
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- src/via_sii164.h
+++ src/via_sii164.h
@@ -3,6 +3,7 @@
  * Copyright 2016 The OpenChrome Project
  *                [http://www.freedesktop.org/wiki/Openchrome]
  * Copyright 2014 SHS SERVICES GmbH
+ * Copyright 2006-2009 Luc Verhaegen.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
--- src/via_vt1632.h
+++ src/via_vt1632.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 SHS SERVICES GmbH
+ * Copyright 2006-2009 Luc Verhaegen.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
