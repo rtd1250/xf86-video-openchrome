rtd1250/xf86-video-openchrome

commit 873c352d728837d0423d4df4cb5e1330591e1ddb

Remove ViaLVDSPowerChannel
Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_fp.c
@@ -880,23 +880,6 @@ ViaLVDSHardwarePowerSecondSequence(ScrnInfoPtr pScrn, Bool on)
 }
 
 static void
-ViaLVDSPowerChannel(ScrnInfoPtr pScrn, Bool on)
-{
-    vgaHWPtr hwp = VGAHWPTR(pScrn);
-    CARD8 lvdsMask;
-
-    if (on) {
-        /* LVDS0: 0x7F, LVDS1: 0xBF */
-        lvdsMask = 0x7F & 0xBF;
-        hwp->writeCrtc(hwp, 0xD2, hwp->readCrtc(hwp, 0xD2) & lvdsMask);
-    } else {
-        /* LVDS0: 0x80, LVDS1: 0x40 */
-        lvdsMask = 0x80 | 0x40;
-        hwp->writeCrtc(hwp, 0xD2, hwp->readCrtc(hwp, 0xD2) | lvdsMask);
-    }
-}
-
-static void
 viaFPPower(ScrnInfoPtr pScrn, Bool powerState, CARD8 diPortType)
 {
     VIAPtr pVia = VIAPTR(pScrn);

commit 3514f8ee923442aa8fdb7a564775bdccda3f12fa

Fix for FP not getting restored when resuming from standby
Commit 873c352 caused a regression in restoring FP on some models
(CX700 chipset through VX900 chipset). This commit should fix the
bug.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_fp.c
@@ -908,19 +908,24 @@ viaFPPower(ScrnInfoPtr pScrn, Bool powerState, CARD8 diPortType)
         if ((diPortType & VIA_DI_PORT_LVDS1)
             && (diPortType & VIA_DI_PORT_LVDS2)) {
             ViaLVDSSoftwarePowerFirstSequence(pScrn, powerState);
+            viaLVDS1SetPower(pScrn, powerState);
             ViaLVDSSoftwarePowerSecondSequence(pScrn, powerState);
+            viaLVDS2SetPower(pScrn, powerState);
         } else if ((diPortType & VIA_DI_PORT_LVDS1)
                     && (~(diPortType & VIA_DI_PORT_LVDS2))) {
             ViaLVDSSoftwarePowerFirstSequence(pScrn, powerState);
+            viaLVDS1SetPower(pScrn, powerState);
         } else if ((~(diPortType & VIA_DI_PORT_LVDS1))
                         && (diPortType & VIA_DI_PORT_LVDS2)) {
             ViaLVDSSoftwarePowerSecondSequence(pScrn, powerState);
+            viaLVDS2SetPower(pScrn, powerState);
         }
 
          break;
     case VIA_VX855:
     case VIA_VX900:
         ViaLVDSHardwarePowerFirstSequence(pScrn, powerState);
+        viaLVDS1SetPower(pScrn, powerState);
         break;
     default:
         break;