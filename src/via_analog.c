rtd1250/xf86-video-openchrome

commit b9bce1cd1316c059cf59c04d10d21103d9316de0

Made modifications to via_analog_prepare
Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_analog.c
@@ -303,8 +303,8 @@ via_analog_prepare(xf86OutputPtr output)
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Entered via_analog_prepare.\n"));
 
-    /* DPMS On */
-    viaAnalogSetDPMSControl(pScrn, 0x03);
+    viaAnalogSetDPMSControl(pScrn, VIA_ANALOG_DPMS_ON);
+    viaAnalogPower(pScrn, TRUE);
 
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Exiting via_analog_prepare.\n"));

commit 29ac0ebb59a4eb70ed4c5ce872e9fb75038f868d

Fix for analog (VGA) screen going off when changing screen resolution
This commit fixes a regression introduced by commits b9bce1c and
d3d03bb was observed on VX700 chipset and VX900 chipset, but not
VN896 chipset.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_analog.c
@@ -303,8 +303,8 @@ via_analog_prepare(xf86OutputPtr output)
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Entered via_analog_prepare.\n"));
 
-    viaAnalogSetDPMSControl(pScrn, VIA_ANALOG_DPMS_ON);
-    viaAnalogPower(pScrn, TRUE);
+    viaAnalogSetDPMSControl(pScrn, VIA_ANALOG_DPMS_OFF);
+    viaAnalogPower(pScrn, FALSE);
 
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Exiting via_analog_prepare.\n"));
@@ -318,8 +318,8 @@ via_analog_commit(xf86OutputPtr output)
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Entered via_analog_commit.\n"));
 
-    viaAnalogSetDPMSControl(pScrn, VIA_ANALOG_DPMS_OFF);
-    viaAnalogPower(pScrn, FALSE);
+    viaAnalogSetDPMSControl(pScrn, VIA_ANALOG_DPMS_ON);
+    viaAnalogPower(pScrn, TRUE);
 
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Exiting via_analog_commit.\n"));