rtd1250/xf86-video-openchrome

commit a245948a455de13719ac6db13608b6cecad186de

Map FB during screen initialization rather than pre-initialization
Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_driver.c
@@ -1216,7 +1216,6 @@ VIAPreInit(ScrnInfoPtr pScrn, int flags)
     goto exit;
 fail:
     if (!pVia->KMS) {
-        viaUnmapFB(pScrn);
         viaUnmapMMIO(pScrn);
     }
 

src/via_ums.c
@@ -806,6 +806,14 @@ viaUMSCreate(ScrnInfoPtr pScrn)
     }
 #endif
 
+    /*
+     * Map FB PCI hardware resource to the memory map.
+     */
+    if (!viaMapFB(pScrn)) {
+        ret = FALSE;
+        goto exit;
+    }
+
     if (pVia->directRenderingType == DRI_NONE) {
         if (!pVia->useEXA) {
             if (!viaInitFB(pScrn)) {
@@ -1139,11 +1147,6 @@ viaUMSPreInit(ScrnInfoPtr pScrn)
 
     /* Map PCI hardware resources to the memory map. */
     if (!viaMapMMIO(pScrn)) {
-        return FALSE;
-    }
-
-    if (!viaMapFB(pScrn)) {
-        viaUnmapMMIO(pScrn);
         return FALSE;
     }
 

commit 838d7951eb9b2d48e2cb990221324c5d9d1682f5

Fix for DRI1 initialization failure
commit a245948 (Map FB during screen initialization rather than
pre-initialization) broke DRI1 initialization code.  This was due to
frame buffer no longer being mapped prior to DRI1 initialization.  Now,
frame buffer is mapped prior to DRI1 initialization.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

src/via_driver.c
@@ -1480,6 +1480,12 @@ VIAScreenInit(SCREEN_INIT_ARGS_DECL)
     /* Must be after RGB ordering is fixed. */
     fbPictureInit(pScreen, NULL, 0);
 
+    if (!pVia->KMS) {
+        if (!viaUMSMapIOResources(pScrn)) {
+            return FALSE;
+        }
+    }
+
 #ifdef HAVE_DRI
     if (pVia->KMS) {
         if (drmSetMaster(pVia->drmmode.fd)) {
@@ -1506,7 +1512,7 @@ VIAScreenInit(SCREEN_INIT_ARGS_DECL)
     if (pVia->directRenderingType != DRI_2)
 #endif /* HAVE_DRI */
     {
-        if (!viaUMSCreate(pScrn)) {
+        if (!viaUMSScreenInit(pScrn)) {
             return FALSE;
         }
     }

src/via_ums.c
@@ -778,7 +778,7 @@ exit:
 }
 
 Bool
-viaUMSCreate(ScrnInfoPtr pScrn)
+viaUMSMapIOResources(ScrnInfoPtr pScrn)
 {
     VIAPtr pVia = VIAPTR(pScrn);
     Bool ret = TRUE;
@@ -822,6 +822,34 @@ viaUMSCreate(ScrnInfoPtr pScrn)
         goto exit;
     }
 
+exit:
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+                        "Exiting %s.\n", __func__));
+    return ret;
+}
+
+void
+viaUMSDestroy(ScrnInfoPtr pScrn)
+{
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+                        "Entered %s.\n", __func__));
+
+    viaUnmapFB(pScrn);
+    viaUnmapMMIO(pScrn);
+
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+                        "Exiting %s.\n", __func__));
+}
+
+Bool
+viaUMSScreenInit(ScrnInfoPtr pScrn)
+{
+    VIAPtr pVia = VIAPTR(pScrn);
+    Bool ret = TRUE;
+
+    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+                        "Entered %s.\n", __func__));
+
     if (pVia->directRenderingType == DRI_NONE) {
         if (!pVia->useEXA) {
             if (!viaInitFB(pScrn)) {
@@ -852,19 +880,6 @@ exit:
     DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                         "Exiting %s.\n", __func__));
     return ret;
-}
-
-void
-viaUMSDestroy(ScrnInfoPtr pScrn)
-{
-    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
-                        "Entered %s.\n", __func__));
-
-    viaUnmapFB(pScrn);
-    viaUnmapMMIO(pScrn);
-
-    DEBUG(xf86DrvMsg(pScrn->scrnIndex, X_INFO,
-                        "Exiting %s.\n", __func__));
 }
 
 static Bool

src/via_ums.h
@@ -1558,8 +1558,9 @@ viaSetDisplayScaling(ScrnInfoPtr pScrn, Bool scalingState)
 /* via_ums.c */
 void viaDisableVQ(ScrnInfoPtr pScrn);
 Bool viaUMSAccelInit(ScrnInfoPtr pScrn);
-Bool viaUMSCreate(ScrnInfoPtr pScrn);
+Bool viaUMSMapIOResources(ScrnInfoPtr pScrn);
 void viaUMSDestroy(ScrnInfoPtr pScrn);
+Bool viaUMSScreenInit(ScrnInfoPtr pScrn);
 Bool viaUMSPreInit(ScrnInfoPtr pScrn);
 void viaUMSPreInitExit(ScrnInfoPtr pScrn);
 Bool viaUMSCrtcInit(ScrnInfoPtr pScrn);